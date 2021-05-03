570ab4a89bc69e00c72a2cdb39dff85fb899e2c7
William Watson <williampwatson1991@gmail.com>
2021-05-03 19:01:45 -0400

    feat: beer info array add, filled with dummy data, mapped to beer component

--- src/components/Beers.js
+++ src/components/Beers.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import beerArray from '../beerInfo';
 
 const Beers = () => {
   return (
-    <div>
-      <h1>YERRRRRRRR</h1>
+    <div className='container'>
+      {beerArray.map((beer) => (
+        <div className='img-outer'>
+          <span className='textbox'>
+            <h1 className='text'>{beer.name}</h1>
+          </span>
+          <img className='beerimg' src={beer.img} alt='assorted beers' />
+        </div>
+      ))}
     </div>
   );
 };
