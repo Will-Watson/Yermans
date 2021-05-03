924ea897b3586c4a5b433bc9a19900a532986ff2
William Watson <williampwatson1991@gmail.com>
2021-05-03 13:51:20 -0400

    feat: bar name font, sliding drawer implemented

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import clsx from 'clsx';
 
 const useStyles = makeStyles((theme) => ({
-  toolbar: {
-    display: 'flex',
-    justifyContent: 'flex-end',
-  },
   root: {
     flexGrow: 1,
+    '& .MuiPaper-root': {
+      // backgroundColor: '#6930C3',
+    },
+    '& .MuiListItem-root': {
+      paddingTop: '20px',
+      paddingBottom: '20px',
+    },
   },
   menuButton: {
     marginRight: theme.spacing(2),
@@ -20,33 +28,82 @@ const useStyles = makeStyles((theme) => ({
   title: {
     flexGrow: 1,
     marginLeft: theme.spacing(2),
+    fontFamily: 'Cormorant Unicase',
   },
 }));
 
 const NavBar = () => {
   const classes = useStyles();
-  // <div className='navbar'>
-  //   <h3>Yerman's Irish Pub</h3>
-  //   <div className='links'>
-  //     <Link to='/'>Home</Link>
-  //     <Link to='/contact'>Contact</Link>
-  //   </div>
-  // </div>
+
+  const [state, setState] = React.useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false,
+  });
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (
+      event.type === 'keydown' &&
+      (event.key === 'Tab' || event.key === 'Shift')
+    ) {
+      return;
+    }
+    setState({ ...state, [anchor]: open });
+  };
+  const list = (anchor) => (
+    <div
+      className={clsx(classes.list, {
+        [classes.fullList]: anchor === 'top' || anchor === 'bottom',
+      })}
+      role='presentation'
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        <Link to='/' style={{ textDecoration: 'none' }}>
+          <ListItem button style={{ color: 'black' }}>
+            Home
+          </ListItem>
+        </Link>
+        <Link to='/contact' style={{ textDecoration: 'none' }}>
+          <ListItem button style={{ color: 'black' }}>
+            Contact
+          </ListItem>
+        </Link>
+      </List>
+    </div>
+  );
 
   return (
     <AppBar position='static'>
       <Toolbar>
-        <Typography variant='h6' className={classes.title}>
+        <Typography variant='h4' className={classes.title}>
           Yerman's Irish Pub
         </Typography>
-        <IconButton
-          edge='start'
-          className={classes.menuButton}
-          color='inherit'
-          aria-label='menu'
-        >
-          <MenuIcon />
-        </IconButton>
+        <div>
+          {['right'].map((anchor) => (
+            <React.Fragment key={anchor}>
+              <IconButton
+                edge='start'
+                className={classes.menuButton}
+                color='inherit'
+                aria-label='menu'
+                onClick={toggleDrawer(anchor, true)}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Drawer
+                className={classes.root}
+                anchor={anchor}
+                open={state[anchor]}
+                onClose={toggleDrawer(anchor, false)}
+              >
+                {list(anchor)}
+              </Drawer>
+            </React.Fragment>
+          ))}
+        </div>
       </Toolbar>
     </AppBar>
   );
