75e5a69e1a34d204f9127037dfb6d5ede10a185e
William Watson <williampwatson1991@gmail.com>
2021-05-04 11:37:14 -0400

    feat: changed background images. Possible different direction on beer menu page

--- src/components/Beers.js
+++ src/components/Beers.js
@@ -4,16 +4,30 @@ import beerArray from '../beerInfo';
 const Beers = () => {
   return (
     <div className='container'>
-      {beerArray.map((beer) => (
-        <div className='img-outer'>
-          <span className='textbox'>
-            <h1 className='text'>{beer.name}</h1>
-          </span>
-          <img className='beerimg' src={beer.img} alt='assorted beers' />
+      <div className='img-outer'>
+        <h1 className='text'>header</h1>
+      </div>
+      <div className='img-outer'>
+        <div>
+          <h1 className='text'>beer image</h1>
         </div>
-      ))}
+        <div>
+          <h1 className='text'>beer text</h1>
+        </div>
+      </div>
     </div>
   );
 };
 
 export default Beers;
+
+//<div className='container'>
+//   {beerArray.map((beer) => (
+//     <div className='img-outer'>
+//       <span className='textbox'>
+//         <h1 className='text'>{beer.name}</h1>
+//       </span>
+//       <img className='beerimg' src={beer.img} alt='assorted beers' />
+//     </div>
+//   ))}
+// </div>
