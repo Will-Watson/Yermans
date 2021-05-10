420d4bfa60e147c2c9e1075bd648071f3dce4cdc
William Watson <williampwatson1991@gmail.com>
2021-05-10 17:20:31 -0400

    feat: sudo menu design. div boxes with classNames for dictation

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,8 +2,23 @@ import React from 'react';
 
 const Menu = () => {
   return (
-    <div>
-      <h1>YERRRRRRRRRRRRRRR</h1>
+    <div className='container'>
+      <div className='img-outer'>
+        <span className='parchment-outer'>
+          <h1>Our Menu</h1>
+        </span>
+      </div>
+      <div className='img-outer'>
+        <div className='parchment-outer'>
+          <div className='food-course'>Soups</div>
+          <div className='food-title'>French Onion Soup</div>
+          <div className='food-price'>$5.25</div>
+          <div className='food-description'>
+            Yerman's classic french onion soup
+          </div>
+        </div>
+        <div className='parchment-outer'>menu box</div>
+      </div>
     </div>
   );
 };
