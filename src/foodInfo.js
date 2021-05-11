bd1f2717c9b3739ab2277bbd08471c28197ec920
William Watson <williampwatson1991@gmail.com>
2021-05-11 16:53:18 -0400

    feat: added foodInfo file. added soups and salads array

---
+++ src/foodInfo.js
@@ -0,0 +1,26 @@
+const soupsAndSalads = [
+  {
+    name: 'French Onion',
+    price: '$5.25',
+  },
+  {
+    name: 'Lobster Bisque',
+    price: '$5.25',
+  },
+  {
+    name: 'Creamy Mushroom',
+    price: '$4.95',
+  },
+  {
+    name: "Yerman's Creamy Potato",
+    price: '$4.95',
+  },
+  {
+    name: 'House Salad',
+    price: '$11.95',
+  },
+  {
+    name: 'Chef Salad with Grilled Chicken',
+    price: '$16.95',
+  },
+];
