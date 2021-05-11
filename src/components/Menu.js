ea2aa080eb87c1fed9930675522399ec022cb294
William Watson <williampwatson1991@gmail.com>
2021-05-11 19:25:13 -0400

    feat: soups and salads mapped to menu component. CSS design first draft complete

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,4 +1,13 @@
 import React from 'react';
+import {
+  soupsAndSalads,
+  wings,
+  appetizers,
+  burgersAndSandwiches,
+  entrees,
+  sideOrders,
+  childrensMenu,
+} from '../foodInfo';
 
 const Menu = () => {
   return (
@@ -10,12 +19,13 @@ const Menu = () => {
       </div>
       <div className='img-outer'>
         <div className='parchment-outer'>
-          <div className='food-course'>Soups</div>
-          <div className='food-title'>French Onion Soup</div>
-          <div className='food-price'>$5.25</div>
-          <div className='food-description'>
-            Yerman's classic french onion soup
-          </div>
+          <div className='food-course'>Soups and Salads</div>
+          {soupsAndSalads.map((item, i) => (
+            <div className='name-and-price'>
+              <div className='item-name'>{item.name}</div>
+              <div className='item-price'>{item.price}</div>
+            </div>
+          ))}
         </div>
         <div className='parchment-outer'>
           <div className='food-course'>Soups</div>
